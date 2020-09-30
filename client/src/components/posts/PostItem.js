b3984414c24366cb9519c38dcbf9e8bc907f45a1
Sam Jackson <samuelsjackson7@gmail.com>
2020-09-30 14:48:46 -0400

    Single Post Display. created components/post/Post.js to show individual posts for discussion and call getPost action

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -10,6 +10,7 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   toggleLike,
   deletePost,
+  showActions,
 }) => {
   return (
     <div className='post bg-white p-1 my-1'>
@@ -24,32 +25,42 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button
-          onClick={(e) => toggleLike(_id)}
-          type='button'
-          className='btn btn-light'
-        >
-          <i className='fas fa-thumbs-up'></i>
-          {likes.length > 0 && <span> {likes.length}</span>}
-        </button>
-        <Link to={`/post/${_id}`} className='btn btn-primary'>
-          Discussion{' '}
-          {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            onClick={(e) => deletePost(_id)}
-            type='button'
-            className='btn btn-danger'
-          >
-            <i className='fas fa-times'></i>
-          </button>
+
+        {showActions && (
+          <Fragment>
+            <button
+              onClick={(e) => toggleLike(_id)}
+              type='button'
+              className='btn btn-light'
+            >
+              <i className='fas fa-thumbs-up'></i>
+              {likes.length > 0 && <span> {likes.length}</span>}
+            </button>
+            <Link to={`/posts/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              {comments.length > 0 && (
+                <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+              <button
+                onClick={(e) => deletePost(_id)}
+                type='button'
+                className='btn btn-danger'
+              >
+                <i className='fas fa-times'></i>
+              </button>
+            )}
+          </Fragment>
         )}
       </div>
     </div>
   );
+};
+
+// showActions was added so that the buttons to like, discuss, delete only show on the Posts page, not the individual Post page (when in discussion). Declared false when passed on Post.js
+PostItem.defaultProps = {
+  showActions: true,
 };
 
 PostItem.propTypes = {
